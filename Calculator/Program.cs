f244d336ae0fe0a6197c930ca1fadc0f2dffbafc
djakub44 <djakub44@gmail.com>
2024-10-15 00:10:50 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Calculator
+{
+    public static double DoOperation(double num1,double num2,string op)
+    {
+        double result = double.NaN;
+        switch(op)
+        {
+            case "a":
+                result = num1 + num2;
+                break;
+            case "s":
+                result = num1 - num2;
+                break;
+            case "m":
+                result = num1 * num2;
+                break;
+            case "d":
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+        
+
+        while (!endApp)
+        {
+            Console.WriteLine("-----Calculator-----\r");
+            Console.WriteLine("----*          *----\n");
+
+            string? numInput1;
+            string? numInput2;
+            double result = 0;
+
+            Console.Write("Type a number, and then press Enter: ");
+            numInput1 = Console.ReadLine();
+
+            double cleanNum1;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.Write("This is not valid input. Please enter a numeric value: ");
+                numInput1 = Console.ReadLine();
+            }
+
+            Console.Write("Type another number, and then press Enter: ");
+            numInput2 = Console.ReadLine();
+
+            double cleanNum2;
+            while (!double.TryParse(numInput2, out cleanNum2))
+            {
+                Console.Write("This is not valid input. Please enter a numeric value: ");
+                numInput2 = Console.ReadLine();
+            }
+
+            Console.WriteLine("----*          *----");
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            string? op = Console.ReadLine();
+            if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+            {
+                Console.WriteLine("Unrecognized input");
+            }
+            else
+            {
+                try
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("It will result in a mathematical error.\n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An exception has occured: " + e.Message);
+                }
+
+            }
+
+
+
+            Console.Write("Press Q and enter to close Calculator console app or any other key to continue...");
+
+            if (Console.ReadLine() == "q" || Console.ReadLine() == "Q") endApp = true;
+            Console.WriteLine("\n");
+        }
+        return;
+    }
+}
+    
